6415445fdab6b386d567b0a7d61af7a6b99a4b46
Willy Huang <willyhuang18@gmail.com>
2022-10-02 00:33:55 -0700

    change styling

--- components/BackgroundCircles.jsx
+++ components/BackgroundCircles.jsx
@@ -18,8 +18,8 @@ function BackgroundCircles() {
       className="relative flex justify-center items-center"
     >
       <div className="absolute border border-[#333333] rounded-full h-[200px] w-[200px] mt-52 animate-ping" />
-      <div className="border border-[#333333] rounded-full h-[300px] w-[300px] mt-52 absolute" />
-      <div className="absolute border border-[#333333] rounded-full h-[500px] w-[500px] mt-52" />
+      <div className="border border-[#333333] rounded-full h-[300px] w-[300px] mt-52 absolute animate-ping" />
+      <div className="absolute border border-[#333333] rounded-full h-[500px] w-[500px] mt-52 animate-ping" />
       <div className="absolute border border-[#F7AB0A] rounded-full opacity-20 h-[650px] w-[650px] mt-52 animate-pulse" />
       <div className="absolute border border-[#333333] rounded-full opacity-20 h-[800px] w-[800px] mt-52" />
     </motion.div>
